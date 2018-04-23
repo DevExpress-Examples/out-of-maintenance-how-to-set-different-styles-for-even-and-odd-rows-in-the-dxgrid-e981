520afc5a60e2f5aca949b006c06976a3b5e2d14f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace OddEvenRows {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+
+    public class SimpleData {
+        public string Column1 { get; set; }
+        public int Column2 { get; set; }
+
+        public SimpleData() { }
+
+        public SimpleData(string str1, int int2) {
+            Column1 = str1;
+            Column2 = int2;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to set different styles for even and odd rows in the DXGrid
+
+
+<p>The following example demonstrates how to mark odd and even rows in a grid with different background colors.<br /><br /><br /></p>
+
+
+<h3>Description</h3>
+
+<p>To accomplish this task, it is necessary to declare an <strong>OddEvenRowStyle</strong>, with different colors for odd and even rows, and set it to the <strong>TableView</strong>&#39;s <strong>RowStyle</strong>.</p>
+
+<br/>
+
+
