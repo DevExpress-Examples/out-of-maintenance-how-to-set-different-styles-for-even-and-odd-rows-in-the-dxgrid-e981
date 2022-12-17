5bae597182f3cf0a25d8fb169ca203d0ab1b495c
DevExpressExampleBot <fakeEmail>
2022-12-17 12:13:29 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to set different styles for even and odd rows in the DXGrid
 ' 
 ' The following example demonstrates how to mark odd and even rows in a grid with
@@ -6,21 +6,15 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E981
-
-
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
 Imports System.Windows
 
 Namespace OddEvenRows
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to set different styles for even and odd rows in the DXGrid
 ' 
 ' The following example demonstrates how to mark odd and even rows in a grid with
@@ -6,46 +6,30 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E981
-
-
-Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace OddEvenRows
 
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits Window
 
-	Public Class SimpleData
-		Private privateColumn1 As String
-		Public Property Column1() As String
-			Get
-				Return privateColumn1
-			End Get
-			Set(ByVal value As String)
-				privateColumn1 = value
-			End Set
-		End Property
-		Private privateColumn2 As Integer
-		Public Property Column2() As Integer
-			Get
-				Return privateColumn2
-			End Get
-			Set(ByVal value As Integer)
-				privateColumn2 = value
-			End Set
-		End Property
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-		Public Sub New()
-		End Sub
+    Public Class SimpleData
 
-		Public Sub New(ByVal str1 As String, ByVal int2 As Integer)
-			Column1 = str1
-			Column2 = int2
-		End Sub
-	End Class
+        Public Property Column1 As String
+
+        Public Property Column2 As Integer
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal str1 As String, ByVal int2 As Integer)
+            Column1 = str1
+            Column2 = int2
+        End Sub
+    End Class
 End Namespace
